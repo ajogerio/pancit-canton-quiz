964702a191089e35dbbb1eea26549925c3c121f7
aritako <atienzabusiness@gmail.com>
2025-04-25 00:23:05 +0800

    feat: create custom component demo

--- src/features/home/HomePage.jsx
+++ src/features/home/HomePage.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import CustomComponent from './components/CustomComponent';
 
 export default function HomePage() {
-  return <h1>Home Page</h1>;
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 gap-4">
+      <h1 className="text-3xl">Welcome to the Pancit Canton Quiz!</h1>
+      <CustomComponent caption="This is a Custom Component, Angelo Ogerio. LETS GOOO!" />
+    </div>
+  );
 }
--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import React from 'react';
-import StartPage from '../features/home/HomePage';
+import HomePage from '../features/home/HomePage';
 import QuizPage from '../features/quiz/QuizPage';
 import ResultsPage from '../features/results/ResultsPage';
 
@@ -9,7 +9,7 @@ export default function AppRouter() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<StartPage />} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/quiz" element={<QuizPage />} />
         <Route path="/results" element={<ResultsPage />} />
       </Routes>
