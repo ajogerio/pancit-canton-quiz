70454e03f5f0c05bac98de064f0034856d5bdb3a
aritako <atienzabusiness@gmail.com>
2025-04-25 00:17:55 +0800

    feat: organize to feature-based folders

--- src/pages/Start.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Start() {
-  return (
-    <h1>Start Page</h1>
-  )
-}
--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,17 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import React from 'react';
-import Start from '../pages/Start';
-import Quiz from '../pages/Quiz';
-import Results from '../pages/Results';
+import StartPage from '../features/home/HomePage';
+import QuizPage from '../features/quiz/QuizPage';
+import ResultsPage from '../features/results/ResultsPage';
 
 export default function AppRouter() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Start />} />
-        <Route path="/quiz" element={<Quiz />} />
-        <Route path="/results" element={<Results />} />
+        <Route path="/" element={<StartPage />} />
+        <Route path="/quiz" element={<QuizPage />} />
+        <Route path="/results" element={<ResultsPage />} />
       </Routes>
     </Router>
   );
