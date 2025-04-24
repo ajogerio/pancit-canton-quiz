f344cada679a324f1d50e7e598e7cd601ab9325b
ajogerio <aaogerio@up.edu.ph>
2025-04-25 01:04:51 +0800

    feat: create Quiz Page components

--- src/features/quiz/QuizPage.jsx
+++ src/features/quiz/QuizPage.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
+import ProgressBar from './components/ProgressBar';
+import Question from './components/Question';
+import Choices from './components/Choices';
 
 export default function QuizPage() {
-  return <h1>Quiz Page</h1>;
+  return (
+    <main className="flex flex-col">
+      <ProgressBar />
+      <Question />
+      <Choices />
+    </main>
+  );
 }
