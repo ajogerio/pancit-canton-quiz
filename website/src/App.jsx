560c42e75e1b2a9bd63084bdb8230e3479dfbbc7
ajogerio <aaogerio@up.edu.ph>
2025-04-24 19:49:59 +0800

    Setup the ReactJS website. Installed TailwindCSS. Changed the font family to Fredoka.

---
+++ website/src/App.jsx
@@ -0,0 +1,11 @@
+  function App() {
+
+    return (
+      <>
+        <h1 className="text-2xl">Hello Pancit Canton</h1>
+        <p> className="weight"</p>
+      </>
+    )
+  }
+
+  export default App
