304e87a4ff2f8760a0c1103e31a9edb24c38e401
ajogerio <aaogerio@up.edu.ph>
2025-04-25 13:47:30 +0800

    feat: add selecting of choices

--- src/features/quiz/components/ChoiceCard.jsx
+++ src/features/quiz/components/ChoiceCard.jsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import React from "react";
+import PropTypes from "prop-types";
 
-export default function ChoiceCard() {
+export default function ChoiceCard({
+  index,
+  choice,
+  selectedChoice,
+  onChoiceClick,
+}) {
+  const choiceLetters = ["A", "B", "C", "D", "E"];
+  const currentLetter = choiceLetters[index];
+
   return (
-    <div>ChoiceCard</div>
-  )
+    <div
+      className={`flex flex-row cursor-pointer rounded-full text-white my-5 gap-1 ${
+        selectedChoice === currentLetter ? "bg-green-400" : "bg-gray-600"
+      }`}
+      onClick={() => {
+        onChoiceClick(currentLetter);
+      }}
+    >
+      <div>{currentLetter}</div>
+      <div>{choice}</div>
+    </div>
+  );
 }
+
+ChoiceCard.propTypes = {
+  index: PropTypes.number,
+  choice: PropTypes.string,
+  selectedChoice: PropTypes.string,
+  onChoiceClick: PropTypes.func,
+};
--- src/features/quiz/components/Choices.jsx
+++ src/features/quiz/components/Choices.jsx
@@ -1,16 +1,30 @@
 import React, { useEffect, useState } from "react";
 import { choicesPerQuestion } from "../data/choices";
-import { ChoiceCard } from "./ChoiceCard";
+import ChoiceCard from "./ChoiceCard";
 
 export default function Choices() {
   // total number of questions
   const questionsTotal = 10;
 
+  // this will hold all the data so it persists
+  const [quizForm, setQuizForm] = useState({
+    1: null,
+    2: null,
+    3: null,
+    4: null,
+    5: null,
+    6: null,
+    7: null,
+    8: null,
+    9: null,
+    10: null,
+  });
+
   // the current question number
   const [questionNumber, setQuestionNumber] = useState(1);
 
   // checks if the user has selected a choice
-  const [hasSelected, setHasSelected] = useState(false);
+  const [selectedChoice, setSelectedChoice] = useState(null);
 
   // current choices to be displayed
   const [choicesToDisplay, setChoicesToDisplay] = useState([]);
@@ -20,18 +34,45 @@ export default function Choices() {
     setChoicesToDisplay(choicesPerQuestion[questionNumber]);
   }, [questionNumber]);
 
+  console.log("Quiz Form:", quizForm);
+
   return (
     <div>
       <div>
         {choicesToDisplay.map((choice, index) => {
-          return <ChoiceCard key={index} choice={choice} />;
+          return (
+            <ChoiceCard
+              key={index}
+              index={index}
+              choice={choice}
+              selectedChoice={selectedChoice}
+              onChoiceClick={(selectedLetter) => {
+                setQuizForm(
+                  (prev) => ({
+                    ...prev,
+                    [questionNumber]: selectedLetter,
+                  }),
+                  setSelectedChoice(selectedLetter)
+                );
+              }}
+            />
+          );
         })}
       </div>
 
       <div>
-        {hasSelected && // if a user has selected a choice
-          questionNumber < questionsTotal - 2 && ( // and the question number is less than the total questions
-            <button className="rounded-full bg-gray-400 text-white p-1">
+        {selectedChoice && // if a user has selected a choice
+          questionNumber < questionsTotal - 1 && ( // and the question number is less than the total questions
+            <button
+              className="rounded-full bg-gray-400 text-white p-1"
+              onClick={() => {
+                setQuestionNumber((prev) => {
+                  const updatedQuestionNumber = prev + 1;
+                  console.log(`New Question Number: ${prev}\n`);
+                  return updatedQuestionNumber;
+                });
+              }}
+            >
               Next Question
             </button>
           )}
