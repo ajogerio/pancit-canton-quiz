8eaa246be936466989393bc86d4101e129a301b6
ajogerio <aaogerio@up.edu.ph>
2025-04-25 10:23:03 +0800

    feat: add prop types to the progress prop in the ProgressBar component

--- src/features/quiz/components/ProgressBar.jsx
+++ src/features/quiz/components/ProgressBar.jsx
@@ -1,10 +1,17 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 export default function ProgressBar({ progress = 50 }) {
   return (
     <div className="h-[20px] min-w-[279px] bg-[#898989] rounded-full">
-      <div className="h-full bg-green-500 rounded-full" style={{width: `${progress}%` }}></div>
+      <div
+        className="h-full bg-green-500 rounded-full"
+        style={{ width: `${progress}%` }}
+      ></div>
     </div>
   );
 }
 
+ProgressBar.propTypes = {
+  progress: PropTypes.number.isRequired,
+};
