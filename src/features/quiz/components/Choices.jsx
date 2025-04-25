cd3c4f2bd947c0efcda1c14d1819d2defe07290c
ajogerio <aaogerio@up.edu.ph>
2025-04-25 12:26:51 +0800

    feat: add data folder that contains the choices per question

--- src/features/quiz/components/Choices.jsx
+++ src/features/quiz/components/Choices.jsx
@@ -1,41 +1,36 @@
 import React, { useEffect, useState } from "react";
+import { choicesPerQuestion } from "../data/choices";
+import { ChoiceCard } from "./ChoiceCard";
 
 export default function Choices() {
   // total number of questions
   const questionsTotal = 10;
 
   // the current question number
-  const [questionNumber, setQuestionNumber] = useState(0);
+  const [questionNumber, setQuestionNumber] = useState(1);
 
   // checks if the user has selected a choice
   const [hasSelected, setHasSelected] = useState(false);
 
   // current choices to be displayed
-  const [choices, setChoices] = useState([]);
+  const [choicesToDisplay, setChoicesToDisplay] = useState([]);
 
   // select the choices to be rendered depending on the question number
   useEffect(() => {
-    const choicesPerQuestion = [
-      ["Choice A", "Choice B", "Choice C", "Choice D", "Choice E"], // question 1
-      ["Choice a", "Choice b", "Choice c", "Choice d", "Choice e"], // question 2
-      ["Choice A", "Choice BA", "Choice KA", "Choice DA", "Choice E"], // question 3
-      // Add more question choices as needed
-    ];
-
-    setChoices(choicesPerQuestion[questionNumber]);
+    setChoicesToDisplay(choicesPerQuestion[questionNumber]);
   }, [questionNumber]);
 
   return (
     <div>
       <div>
-        {choices.map((choice, index) => {
+        {choicesToDisplay.map((choice, index) => {
           return <ChoiceCard key={index} choice={choice} />;
         })}
       </div>
 
       <div>
         {hasSelected && // if a user has selected a choice
-          questionNumber < questionsTotal - 1 && ( // and the question number is less than the total questions
+          questionNumber < questionsTotal - 2 && ( // and the question number is less than the total questions
             <button className="rounded-full bg-gray-400 text-white p-1">
               Next Question
             </button>
---
+++ src/features/quiz/data/choices.js
@@ -0,0 +1,6 @@
+export const choicesPerQuestion = {
+  1 : ["Choice A", "Choice B", "Choice C", "Choice D", "Choice E"],
+  2 : ["Choice A", "Choice B", "Choice C", "Choice D", "Choice E"],
+  3 : ["Choice A", "Choice B", "Choice C", "Choice D", "Choice E"],
+}
+
