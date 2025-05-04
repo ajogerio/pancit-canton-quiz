f9873311880a1c9c9ea62318caadd48dccc5c00d
ajogerio <aaogerio@up.edu.ph>
2025-05-05 00:59:08 +0800

    feat: make the text and button responsive

--- src/features/home/HomePage.jsx
+++ src/features/home/HomePage.jsx
@@ -6,20 +6,20 @@ export default function HomePage() {
   const navigate = useNavigate();
 
   return (
-    <main className="flex items-center justify-center min-h-screen bg-white gap-4 px-60">
+    <main className="flex flex-col lg:flex-row items-center justify-center min-h-screen bg-white gap-4 px-60 p-20">
       <div className="flex flex-col gap-5">
-        <h1 className="text-6xl">
+        <h1 className="text-6xl text-center lg:text-left">
           Welcome to the <br></br>
           <span className="text-[#1a8954]">Pancit Canton</span> Quiz!
         </h1>
-        <h2 className="text-xl text-gray-600">
+        <h2 className="text-xl text-gray-600 text-center lg:text-left">
           Embark on a flavorful adventure and discover which Pancit Canton
           flavor matches your personality!
         </h2>
         <button
-          className="text-start bg-[#1a8954] text-white w-fit 
+          className="mx-auto bg-[#1a8954] text-white w-fit 
         shadow-xl px-10 py-2 rounded-md hover:cursor-pointer 
-         hover:bg-[#166e45] border-2 border-[#1a8954]  active:bg-[#115f38]"
+         hover:bg-[#166e45] border-2 border-[#1a8954]  active:bg-[#115f38] lg:mx-0"
           onClick={() => {
             navigate("/quiz");
           }}
@@ -28,7 +28,10 @@ export default function HomePage() {
         </button>
       </div>
       <div>
-        <img src="/home-page-image-1.png"></img>
+        <img
+          className="h-120 w-150 object-contain"
+          src="/home-page-image-1.png"
+        ></img>
       </div>
     </main>
   );
