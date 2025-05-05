0507cbdeef2d7ad42347c04078e8d65379c3d7f9
ajogerio <aaogerio@up.edu.ph>
2025-05-05 12:28:03 +0800

    feat: add calculating page and pan moving animation

--- src/features/calculatingResults/components/CalculatingResultsPage.jsx
+++ src/features/calculatingResults/components/CalculatingResultsPage.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React from "react";
 
 export default function CalculatingResultsPage() {
   return (
-    <div>CalculatingResultsPage</div>
-  )
+    <main className="flex flex-col items-center justify-center min-h-screen">
+      <div>
+        <img src="/pan-1.png" className="w-50 h-50 animate-toss"></img>
+      </div>
+      <div><p className="text-2xl">Tindera is cooking . . .</p></div>
+    </main>
+  );
 }
