ff5855b33c6a4697be9ba468ea6c782119be0478
ajogerio <aaogerio@up.edu.ph>
2025-05-15 15:05:39 +0800

    feat: add reveal section

--- src/features/results/ResultsPage.jsx
+++ src/features/results/ResultsPage.jsx
@@ -1,5 +1,25 @@
-import React from 'react';
+import React from "react";
+import "../../index.css";
 
 export default function ResultsPage() {
-  return <h1>Results Page</h1>;
+  return (
+    <main>
+      <section>
+        <div
+          className="relative flex flex-col justify-center items-center min-h-screen 
+        w-full bg-[url(/public/home-page-image-1.png)] bg-center bg-repeat"
+        >
+          <div className="absolute inset-0 bg-black/60"></div>
+          <div className="flex flex-col justify-center items-center relative z-10 text-white">
+            <p className="text-5xl">You are</p>
+            <h1 className="text-9xl">KALAMANSI</h1>
+            <p className="text-5xl">flavor</p>
+          </div>
+        </div>
+      </section>
+      <section>
+
+      </section>
+    </main>
+  );
 }
