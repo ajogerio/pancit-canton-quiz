e8be9a71f62f03090ce641eca4c2795a2f7defcf
ajogerio <aaogerio@up.edu.ph>
2025-05-19 23:06:56 +0800

    feat: make the page responsive

--- src/features/discussion/components/FlavorDescription.jsx
+++ src/features/discussion/components/FlavorDescription.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function FlavorDescription({ children }) {
   return (
-    <div className="flex flex-col justify-center items-center gap-2 py-30 px-30">
+    <div className="flex flex-col justify-center items-center gap-2 py-30 px-10 md:px-30">
       {children}
     </div>
   );
