1b0272a8ff11e23dec4ed7aa3144cf356c95f4d8
ajogerio <aaogerio@up.edu.ph>
2025-05-19 15:25:34 +0800

    feat: route result page to discussion page

---
+++ src/features/discussion/DiscussionPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function DiscussionPage() {
+  return (
+    <div>DiscussionPage</div>
+  )
+}
--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,13 +1,14 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import React from 'react';
-import HomePage from '../features/home/HomePage';
-import QuizPage from '../features/quiz/QuizPage';
-import CalculatingResultsPage from '../features/calculatingResults/components/CalculatingResultsPage';
-import ExtraHotChiliPage from '../features/results/ExtraHotChiliPage';
-import KalamansiPage from '../features/results/KalamansiPage';
-import OriginalPage from '../features/results/OriginalPage';
-import SweetAndSpicyPage from '../features/results/SweetAndSpicyPage';
+import React from "react";
+import HomePage from "../features/home/HomePage";
+import QuizPage from "../features/quiz/QuizPage";
+import CalculatingResultsPage from "../features/calculatingResults/components/CalculatingResultsPage";
+import ExtraHotChiliPage from "../features/results/ExtraHotChiliPage";
+import KalamansiPage from "../features/results/KalamansiPage";
+import OriginalPage from "../features/results/OriginalPage";
+import SweetAndSpicyPage from "../features/results/SweetAndSpicyPage";
+import DiscussionPage from "../features/discussion/DiscussionPage";
 
 export default function AppRouter() {
   return (
@@ -20,6 +21,7 @@ export default function AppRouter() {
         <Route path="/results/sweetandspicy" element={<SweetAndSpicyPage />} />
         <Route path="/results/extrahotchili" element={<ExtraHotChiliPage />} />
         <Route path="/results/original" element={<OriginalPage />} />
+        <Route path="/discussion" element={<DiscussionPage />} />
       </Routes>
     </Router>
   );
