64793f708283c853bb858e26cbaa22e94bc145b2
Ma. Pauline Abcede <97336249+abcpau@users.noreply.github.com>
2025-05-20 11:40:40 +0800

    chore: download images

--- src/features/quiz/data/choiceImages.js
+++ src/features/quiz/data/choiceImages.js
@@ -1,62 +1,62 @@
 export const choiceImages = {
   1: {
-    A: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhae4dDAO65lsY3jEPAE-Uy6tatqCZvOJNjoZASDaLMTJA9D2W9ut3yNTlMY54pb-3xclHgbRkQfxNDGHfpiaTQDZWl1vn0iBvZNTumjVliFVmzlifsQtrxXPvtulonXBK6amX8kx4Oy5kY/s640/z+%25284%2529.JPG", // Oblation
-    B: "https://scontent.fmnl17-5.fna.fbcdn.net/v/t1.6435-9/65041995_2775605852514298_8906987995458961408_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=f727a1&_nc_eui2=AeFOLToRbbScmEhGgtx79z5vgnOC3zntjWuCc4LfOe2NazuPC9Z-1eTO0t9X_6iOOoErgyqnCSi3HBb6dYe8y_Os&_nc_ohc=fMCAO10K47YQ7kNvwGYT8Cu&_nc_oc=AdlKPSjdGN9f3dtzm-tTYPK8HGc0ouomPL_QGrrwunEvxQwLpYXJB8CDg0Mee4YCDqo&_nc_zt=23&_nc_ht=scontent.fmnl17-5.fna&_nc_gid=LRjL9ouf9faHaq2YF84XaQ&oh=00_AfLeH8xExGjGtw6DitYhEwRx-Cqo3eVTHeuzoCIz-U2caA&oe=68525BD8", // Sunflowers
-    C: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgJYpijj6yGGpY71xurzHYh7iaCxOJcsfP8OQidgKI_vy8tZepu1GR2q10deAvrBKToxBMmcQrMdwHdtymPuIVTMXQshSJZIrt-YBv40Hxrb_U4Sk-WsJmdgClWDNELcXK6HevvL4Bs5ZM/s640/P2203501_1.jpg", // Ikot
-    D: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/17/UP_Diliman_Quezon_Hall.jpg/1200px-UP_Diliman_Quezon_Hall.jpg?20130406201219", // Quezon Hall
+    A: "/1a.JPG", // Oblation
+    B: "/1b.jpg", // Sunflowers
+    C: "/1c.jpg", // Ikot
+    D: "/1d.jpg", // Quezon Hall
   },
   2: {
-    A: "https://images.unsplash.com/photo-1558418294-9da149757efe?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", // Sky
-    B: "https://santosmauii.wordpress.com/wp-content/uploads/2014/02/img_17891.jpg", // Grass
-    C: "https://plus.unsplash.com/premium_photo-1674834298045-e405bc99076b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", // Clouds
-    D: "https://images.unsplash.com/photo-1594315590298-329f49c8dcb9?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", // Sun
+    A: "/2a.jpg", // Sky
+    B: "/2b.jpg", // Grass
+    C: "/2c.jpg", // Clouds
+    D: "/2d.jpg", // Sun
   },
   3: {
-    A: "https://compote.slate.com/images/277a0417-dc8b-4aa4-88a8-9d87a4420c0b.jpeg?crop=4120%2C2746%2Cx0%2Cy91", // Comic Sans
-    B: "https://i.etsystatic.com/27972996/r/il/0a36f0/4164122473/il_1080xN.4164122473_cuxe.jpg", // TNR
-    C: "https://cdn.osxdaily.com/wp-content/uploads/2021/04/how-to-enable-headphone-notifications-apple-watch.jpg", // sans Serif
-    D: "https://images.squarespace-cdn.com/content/573f3af337013b783febc526/1463762331687-HXY9TAUVKCX4HSSYFXDV/IMG_4423.JPG?content-type=image%2Fjpeg", // Nautica
+    A: "/3a.avif", // Comic Sans
+    B: "/3b.webp", // TNR
+    C: "/3c.jpg", // sans Serif
+    D: "/3d.jpg", // Nautica
   },
   4: {
-    A: "https://i.pinimg.com/736x/22/fd/9a/22fd9ad59a2df6718465e20e4677b1b0.jpg", // Motivated
-    B: "https://pocarisweat.ng/wp-content/uploads/2023/12/shutterstock_2262323487-1.jpg", // Bakit ba
-    C: "https://as2.ftcdn.net/jpg/04/79/82/87/1000_F_479828722_QOyfXABWmublZXvBNzxwwyXgMUlHCkdz.jpg", // Ahh
-    D: "https://sinag.press/news/wp-content/uploads/2024/04/435279848_428445426454528_1506991794276407864_n-1.jpg", // Kakain
+    A: "/4a.jpg", // Motivated
+    B: "/4b.jpg", // Bakit ba
+    C: "/4c.jpg", // Ahh
+    D: "/4d.jpg", // Kakain
   },
   5: {
-    A: "https://i.pinimg.com/736x/f4/9d/d8/f49dd885a72854a07ab8424976fe0dfb.jpg", // Straight
-    B: "https://i.pinimg.com/1200x/3c/d9/7b/3cd97bdbf1bf556df334b8488ba2ab7b.jpg", // Depth
-    C: "https://upd.edu.ph/wp-content/uploads/2022/03/road-closure-shari-ah-bar.jpg", // Oval
-    D: "https://scontent.fmnl17-1.fna.fbcdn.net/v/t39.30808-6/480570138_640031771857406_5570197124058838856_n.jpg?stp=cp6_dst-jpg_tt6&_nc_cat=100&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeHI3_6U-hJ_-_3RRGzAKbC-8xn14nnO91fzGfXiec73V2PQhVLwlErdjr-TJl9tGqpjUHSUZ6xYbODmLS-j2jbi&_nc_ohc=OIiVCJSrx5kQ7kNvwFlphFn&_nc_oc=AdncXJyzUBfRsqHEUP0EAHGzcBUWWmMiZFKYT4tz8VH2OKpu71dPLI14Kz2g9pDvCd8&_nc_zt=23&_nc_ht=scontent.fmnl17-1.fna&_nc_gid=yZOzMY_tB9sT3tPMPAiPVw&oh=00_AfLZjWvn3lWGZmEkegfFu7RPjYO6rKVP6KaIZSL_FF-mSQ&oe=6830DC0C", // Football
+    A: "/5a.jpg", // Straight
+    B: "/5b.jpg", // Depth
+    C: "/5c.jpg", // Oval
+    D: "/5d.jpg", // Football
   },
   6: {
-    A: "https://images.unsplash.com/photo-1546860255-95536c19724e", // Sili
-    B: "https://epicureanearth.com/wp-content/uploads/2024/09/all-about-the-calamansi-2048x1365.jpg", // Calamansi
-    C: "https://images.unsplash.com/photo-1636210589096-a53d5dacd702", // Garlic
-    D: "https://preview.redd.it/scrambled-eggs-cocktail-hotdogs-v0-vosoa09i9vmd1.jpeg?width=1080&crop=smart&auto=webp&s=daf7b5fc3980c3f0e6ecc8cccb3c030a9c795d4e", // Itlog at Hotdog
+    A: "/6a.jpg", // Sili
+    B: "/6b.jpg", // Calamansi
+    C: "/6c.jpg", // Garlic
+    D: "/6d.webp", // Itlog at Hotdog
   },
   7: {
-    A: "https://themayakitchen.com/wp-content/uploads/2020/09/Kwek-%E2%80%93-Kwek-7.jpg", // Kwek-Kwek
-    B: "https://preview.redd.it/homemade-pork-siomai-v0-xwyejaw33rm81.jpg?width=1080&crop=smart&auto=webp&s=0ea8a98609bae6b163fb9c571ec547083dde6881", // Siomai
-    C: "https://www.kawalingpinoy.com/wp-content/uploads/2020/05/crispy-cheese-sticks-4-1152x1536.jpg", // Cheese sticks
-    D: "https://www.kawalingpinoy.com/wp-content/uploads/2015/04/crispy-dynamite-lumpia-3-1152x1536.jpg", // Dynamite
+    A: "/7a.jpg", // Kwek-Kwek
+    B: "/7b.webp", // Siomai
+    C: "/7c.jpg", // Cheese sticks
+    D: "/7d.jpg", // Dynamite
   },
   8: {
-    A: "https://img.freepik.com/free-vector/all-right-emoji-illustration_23-2151298395.jpg?semt=ais_hybrid&w=740", // Tama
-    B: "https://i.pinimg.com/736x/40/49/37/404937b386ba448c4faceb2d2711d046.jpg", // Mali
-    C: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSAGkYoFFOV_IfSWED5YoBGZnNrqOC8nVq9QTYC2sdVawFWBuERKe8oYEC1a4CrNWTgfqg&usqp=CAU", // Maybe
-    D: "hhttps://images.emojiterra.com/google/noto-emoji/unicode-16.0/color/1024px/1f914.png", // Mmmm
+    A: "/8a.avif", // Tama
+    B: "/8b.jpg", // Mali
+    C: "/8c.jpg", // Maybe
+    D: "/8d.png", // Mmmmmm
   },
   9: {
-    A: "https://almerepinoystore.com/wp-content/uploads/2021/05/Lucky-Me-Pancit-Canton-Original-1.png", // Original
-    B: "https://almerepinoystore.com/wp-content/uploads/2021/05/Lucky-Me-Pancit-Canton-Extra-Hot-Chili.png", // Spicy
-    C: "https://almerepinoystore.com/wp-content/uploads/2023/04/lucky-me-sweet-and-spicy.png", // Sweet and Spicy
-    D: "https://almerepinoystore.com/wp-content/uploads/2021/05/Lucky-Me-Pancit-Canton-Kalamansi.png", // Kalamansi
+    A: "/9a.png", // Original
+    B: "/9b.png", // Spicy
+    C: "/9c.png", // Sweet and Spicy
+    D: "/9d.png", // Kalamansi
   },
   10: {
-    A: "https://themayakitchen.com/wp-content/uploads/2020/09/Kwek-%E2%80%93-Kwek-7.jpg", // Kwek-Kwek
-    B: "https://preview.redd.it/homemade-pork-siomai-v0-xwyejaw33rm81.jpg?width=1080&crop=smart&auto=webp&s=0ea8a98609bae6b163fb9c571ec547083dde6881", // Siomai
-    C: "https://www.kawalingpinoy.com/wp-content/uploads/2020/05/crispy-cheese-sticks-4-1152x1536.jpg", // Cheese sticks
-    D: "https://www.kawalingpinoy.com/wp-content/uploads/2015/04/crispy-dynamite-lumpia-3-1152x1536.jpg", // Dynamite
+    A: "/7a.jpg", // Kwek-Kwek
+    B: "/7b.webp", // Siomai
+    C: "/7c.jpg", // Cheese sticks
+    D: "/7d.jpg", // Dynamite
   },
 };
